37d7e099f819e506200095f097f1abdc30cb6918
ehsan <ehssamimi@gmail.com>
2019-11-02 15:02:26 +0330

    add initial and create submit form

---
+++ landing-chichi/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ import Landing from "./Components/Landing";
+
+function App() {
+  return (
+    <div className="App">
+   <Landing/>
+
+    </div>
+  );
+}
+
+export default App;
