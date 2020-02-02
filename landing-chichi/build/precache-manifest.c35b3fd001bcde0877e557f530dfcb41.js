d10ac6468c76ef8d3e7798173aa66ed83a4a4fc1
aminjamal <aminjamal10@gmail.com>
2020-02-02 12:48:43 +0430

    remove node module

---
+++ landing-chichi/build/precache-manifest.c35b3fd001bcde0877e557f530dfcb41.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b6f8dcaea59f571a6386ef185eb4ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd7a49598920ffe1288",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "6eb2b25db12e42f895f2",
+    "url": "/static/css/main.a932091b.chunk.css"
+  },
+  {
+    "revision": "bcd7a49598920ffe1288",
+    "url": "/static/js/2.287ee313.chunk.js"
+  },
+  {
+    "revision": "6eb2b25db12e42f895f2",
+    "url": "/static/js/main.c756caf3.chunk.js"
+  },
+  {
+    "revision": "5de5c6a783c2ed2dde87",
+    "url": "/static/js/runtime-main.16a2ca73.js"
+  },
+  {
+    "revision": "962a2a6efdc8cc742871c03ab80e78f1",
+    "url": "/static/media/ChichiMan.962a2a6e.png"
+  },
+  {
+    "revision": "c7bcf46d2e2e85ea47da134d28a279ae",
+    "url": "/static/media/Illustration1.c7bcf46d.svg"
+  },
+  {
+    "revision": "a6120f79ddd6825dcc110cedee777b06",
+    "url": "/static/media/Illustration2.a6120f79.svg"
+  },
+  {
+    "revision": "6bdb359c5fbfc24459e4efe4b7d40cd8",
+    "url": "/static/media/bottom.6bdb359c.png"
+  },
+  {
+    "revision": "f98a7dae239a1ba3bd0839ec56986b82",
+    "url": "/static/media/clock-square.f98a7dae.png"
+  },
+  {
+    "revision": "8de297f6d45432a902a59cfddbffe947",
+    "url": "/static/media/header.8de297f6.png"
+  }
+]);
